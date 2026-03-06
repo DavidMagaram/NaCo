ba10f03827fbdb23042db3f45646fb92a399449e
David <d.magaram@tilburguniversity.edu>
2026-03-06 17:49:43 +0100

    report

--- Assignment3/si-exercises/exercise_boids/boids.js
+++ Assignment3/si-exercises/exercise_boids/boids.js
@@ -2,9 +2,9 @@ let canvas, S
 let conf = {
 	w : 400,
 	h : 400,
-	N : 200, 
+	N : 200, // Num of boids
 	zoom : 1,
-	innerRadius : 10,
+	innerRadius : 10, // This and below: Can adjust with sliders
 	outerRadius : 25,
 	cohesion : 1,
 	separation : 1,
