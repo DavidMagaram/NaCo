03923c0fbca8dac20245fcd8b027e38e2afb1a7f
zitr0y <zitroschmidt@gmail.com>
2026-04-14 22:41:03 +0200

    newest changes

--- Assignment4/experiments_b1.py
+++ Assignment4/experiments_b1.py
@@ -1,5 +1,14 @@
+import random
+
 import numpy as np
-from string_ga import run_ga
+from string_ga import (
+    fitness,
+    mutate,
+    random_individual,
+    run_ga,
+    single_point_crossover,
+    tournament_selection,
+)
 
 TARGET = "NaturalComputing"
 
@@ -77,6 +86,49 @@ def experiment_b1_3(n_runs=10, G_max=100, N=200):
         found = sum(1 for r in runs if r["t_finish"] is not None)
         print(f"    Found target in {found}/{n_runs} runs")
     return all_results
+
+
+def experiment_b1_snapshots(
+    G_max=200,
+    N=200,
+    K=2,
+    snap_gens=(0, 10, 30, 60, 100),
+    n_top=4,
+    n_rand=3,
+    seed=7,
+):
+    L = len(TARGET)
+    mu_configs = {
+        "mu=0": 0,
+        "mu=1/L": 1 / L,
+        "mu=3/L": 3 / L,
+    }
+    target_list = list(TARGET)
+    all_snaps = {}
+    rng = random.Random(seed)
+    for label, mu in mu_configs.items():
+        random.seed(rng.random())
+        pop = [random_individual(L) for _ in range(N)]
+        snaps = {}
+        for g in range(G_max):
+            fits = [fitness(ind, target_list) for ind in pop]
+            if g in snap_gens:
+                order = sorted(range(N), key=lambda i: -fits[i])
+                top = [("".join(pop[i]), fits[i]) for i in order[:n_top]]
+                sample_idx = random.sample(range(N), n_rand)
+                rand = [("".join(pop[i]), fits[i]) for i in sample_idx]
+                snaps[g] = {"top": top, "rand": rand}
+            if max(fits) == L:
+                break
+            new_pop = []
+            for _ in range(N // 2):
+                p1 = tournament_selection(pop, fits, K)
+                p2 = tournament_selection(pop, fits, K)
+                c1, c2 = single_point_crossover(p1, p2)
+                new_pop.extend([mutate(c1, mu), mutate(c2, mu)])
+            pop = new_pop
+        all_snaps[label] = snaps
+    return all_snaps
 
 
 def experiment_b1_5(n_runs=10, G_max=100, N=200):
